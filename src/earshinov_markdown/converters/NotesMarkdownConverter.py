09b7d5d2800d4bd286000ccde8c5701b4e59d7da
Eugene Arshinov <earshinov@gmail.com>
2011-10-30 17:52:27 +0400

    Исправление недочётов, выявленных unit-тестами

--- src/earshinov_markdown/converters/NotesMarkdownConverter.py
+++ src/earshinov_markdown/converters/NotesMarkdownConverter.py
@@ -5,9 +5,17 @@ import re
 class NotesMarkdownConverter(ConverterDecorator):
   '''Отдельный запуск Markdown на каждую заметку для локальности ссылок (reference)'''
   
-  __NOTE_HEADER_RE = re.compile(r'\n\n(# .*\n\n)')
+  __NOTE_HEADER_RE = re.compile(r'(?<=\n\n)(# .*)(?=$|\n$|\n\n)')
   
   def convert(self, text):
+    # На случай, если заголовок располагается в первой или второй строке,
+    # чтобы в регулярном выражении сработал lookbehind.  Использовать "|"
+    # в lookbehind, как в lookahead, не получается из-за ошибки
+    # "look-behind requires fixed-width pattern", которую выдаёт Python.
+    # Добавленные символы всё равно ни на что не влияют, потому что вся
+    # разметка до первого заголовка игнорируется
+    text = '\n\n' + text
+    
     blocks = self.__NOTE_HEADER_RE.split(text)
     if not blocks:
       return ''
@@ -16,4 +24,4 @@ class NotesMarkdownConverter(ConverterDecorator):
     it = iter(blocks[1:])
     for header, body in zip(it, it):
       result.append(self._converter.convert(header + body))
-    return '\n'.join(result)
+    return ''.join(result)
