98493220fbe561df27a1d7145e768e077c6799b7
Eugene Arshinov <earshinov@gmail.com>
2011-10-31 11:29:29 +0400

    UrlizeExtension: обеспечили обработку ссылок в угловых скобках (<http://yandex.ru/>).  Раньше они подхватывались шаблончиком html и попадали в результирующий документ без преобразования

--- src/earshinov_markdown/extensions/UrlizeExtension.py
+++ src/earshinov_markdown/extensions/UrlizeExtension.py
@@ -3,19 +3,24 @@ from markdown import Extension
 
 
 URLIZE_RE = '(%s)' % '|'.join([
-    r'<(?:f|ht)tps?://[^>]*>',
+    #r'<(?:f|ht)tps?://[^>]*>',
+      # Обрабатывается стандартным шаблончиком autolink,
+      # см. UrlizeExtension.extendMarkdown
     r'\b(?:f|ht)tps?://[^)<>\s]+[^.,)<>\s]',
     r'\bwww\.[^)<>\s]+[^.,)<>\s]',
-    # Убираем четвёртый/последний паттерн из urlize.URLIZE_RE,
-    # потому что он даёт ненужные срабатывания.  Например в
-    # заметке о cl1p.net он матчит строку "cl1p.net" в заголовке.
+    #r'[^(<\s]+\.(?:com|net|org)\b',
+      # Убираем потому что даёт ненужные срабатывания.  Например в
+      # заметке о cl1p.net он матчит строку "cl1p.net" в заголовке.
 ])
 
 class UrlizeExtension(Extension):
 
   def extendMarkdown(self, md, md_globals):
-    # необходимо добавлять после html, чтобы содержимое атрибута
-    # href элементов a, вручную заданных в разметке Markdown,
-    # не заменялось на placeholder'ы 
-    md.inlinePatterns.add('autolink', UrlizePattern(URLIZE_RE, md), '>html')
+    # 1. необходимо добавлять после html, чтобы содержимое атрибута
+    #    href элементов a, вручную заданных в разметке Markdown,
+    #    не заменялось на placeholder'ы
+    # 2. меняем название с autolink на autolink_changed, потому что
+    #    autolink - стандартный шаблончик, обрабатывающий ссылки в
+    #    угловых скобках, который идёт *до* шаблончика html
+    md.inlinePatterns.add('autolink_changed', UrlizePattern(URLIZE_RE, md), '>html')
 
