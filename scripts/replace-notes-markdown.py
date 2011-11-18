87096225f927677eb54af0d3303b4e8ea0564e52
Eugene Arshinov <earshinov@gmail.com>
2011-11-18 14:37:05 +0400

    MarkdownInsideHtmlExtension: использование в replace-notes-markdown

--- scripts/replace-notes-markdown.py
+++ scripts/replace-notes-markdown.py
@@ -3,7 +3,8 @@
 from earshinov_markdown import FileOperator
 from earshinov_markdown.converters import InlineMarkdownConverter, \
   NotesMarkdownConverter, SimpleMarkdownConverter
-from earshinov_markdown.extensions import DefaultExtensions
+from earshinov_markdown.extensions import DefaultExtensions, \
+  MarkdownInsideHtmlExtension
 from earshinov_markdown.extensions.notes import NotesExtension
 import sys
 
@@ -14,6 +15,7 @@ FileOperator(
   InlineMarkdownConverter(
   NotesMarkdownConverter(
   SimpleMarkdownConverter(DefaultExtensions.get() + [
+    MarkdownInsideHtmlExtension(disabledTreeProcessors=['notesmarkup']),
     NotesExtension(),
   ])))
 ).processConsole(sys.argv)
